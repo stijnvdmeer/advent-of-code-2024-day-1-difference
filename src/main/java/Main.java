c092c430dc58c31b1332e16a227dc51a2e085bb7
stijnvdmeer <stijn08072002@outlook.com>
2025-01-08 17:41:03 +0100

    temp - working on splitting array list in 2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+    public static void main(String[] args) {
+        String inputString = "3 4 4 3 2 5 1 3 3 9 3 3";
+        String[] inputList = inputString.split(" ");
+        List<String> listOne = new ArrayList<String>();
+        List<String> listTwo = new ArrayList<String>();
+
+        boolean listOption = true;
+        for (String e : inputList) {
+
+        }
+
+        // turn input string into an array;
+        // divide the array 1 by 1 into 2 seperate arrays (1 - A1, 2 - A2, 3 - A3 etc
+        // Sort each array on number, lowest to highest
+        // Calculate the difference between the values of the same index of the arrays
+        // Add difference to the total
+        // Output total
+    }
 }
